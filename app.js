7ee636f7246b49e6e7e5a5ed11442379be5dc4cc
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-07-20 15:29:01 -0700

    Initial commit

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+
+var homeStartingContent =
+  'uehfuehuwuhehfueufhuef iehrfeha u8eierhdef ihfeuf aeuuf hueuhea fuueua uebfuahfueheu uebreuiaheuueuha uereuaeaheuea aeuuhrehuuaheue euruehauehue euuhreuahreur eurhehauerhu ueuhrueuauehr';
 
 var app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('Home');
+  res.render('Home', { startingContent: homeStartingContent });
 });
 
 app.listen(3000, function () {
